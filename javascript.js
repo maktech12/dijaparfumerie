3df460f3dc2699bdc48b1893b9045b3acd49d036
Mamadou Abib Kebe <benibrahimkebe@gmail.com>
2025-11-19 09:30:54 +0000

    changes

--- javascript.js
+++ javascript.js
@@ -6,3 +6,40 @@ toggleButton.addEventListener('click', () => {
     // La fonction clé: elle ajoute ou retire la classe 'active'
 navLinks.classList.toggle('active');
 });
+
+
+// Panier code commence ici........................................................
+
+document.getElementById("compte-btn").addEventListener("click", function() {
+    document.querySelector(".fond").style.display = "flex";
+})
+document.getElementById("compte-btn").addEventListener("click", function() {
+    document.querySelector(".popupContenu").style.display = "flex";
+})
+
+document.getElementById("popup-exit").addEventListener("click", function () {
+    document.querySelector(".popupContenu").style.display = "none";
+})
+
+document.getElementById("popup-exit").addEventListener("click", function () {
+    document.querySelector(".fond").style.display = "none";
+})
+
+
+/////
+document.getElementById("inscrire-btn").addEventListener("click", function() {
+    document.querySelector(".inscrire-fond").style.display = "flex";
+    document.querySelector(".popupContenu").style.display = "none";
+})
+document.getElementById("inscrire-btn").addEventListener("click", function() {
+    document.querySelector(".inscrire-popup").style.display = "flex";
+    document.querySelector(".fond").style.display = "none";
+})
+
+document.getElementById("inscrire-popup-exit").addEventListener("click", function () {
+    document.querySelector(".inscrire-fond").style.display = "none";
+})
+
+document.getElementById("inscrire-popup-exit").addEventListener("click", function () {
+    document.querySelector(".inscrire-popup").style.display = "none";
+})
